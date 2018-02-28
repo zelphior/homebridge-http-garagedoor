d9ab980a5e93be454986358c5c5e61b771dd9b6e
mach1009 <matthiasguse@t-online.de>
2018-02-28 14:53:31 +0100

    First release of GarageDoorOpener Plugin
    based on the codetwice/homebridge-http-securitysystem

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var pollingtoevent = require("polling-to-event");
 module.exports = function(homebridge){
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	homebridge.registerAccessory("homebridge-http-securitysystem", "Http-SecuritySystem", HttpSecuritySystemAccessory);
+	homebridge.registerAccessory("homebridge-http-garagedoor", "Http-GarageDoor", HttpGarageDoorAccessory);
 };
 
 /**
@@ -73,19 +73,19 @@ function XPathMapper(parameters) {
 }
 
 /**
- * The main class acting as the Security System Accessory
+ * The main class acting as the garage door Accessory
  *
  * @param log The logger to use
  * @param config The config received from HomeBridge
  * @constructor
  */
-function HttpSecuritySystemAccessory(log, config) {
+function HttpGarageDoorAccessory(log, config) {
 	var self = this;
 	self.log = log;
 	self.name = config["name"];
 
 	// the service
-	self.securityService = null;
+	self.garagedoorService = null;
 
 	// debug flag
 	self.debug = config.debug;
@@ -118,22 +118,6 @@ function HttpSecuritySystemAccessory(log, config) {
 
 	// url info
 	self.urls = {
-		stay: {
-			url: config.urls.stay.url,
-			body: config.urls.stay.body || ""
-		},
-		away: {
-			url: config.urls.away.url,
-			body: config.urls.away.body || ""
-		},
-		night: {
-			url: config.urls.night.url,
-			body: config.urls.night.body || ""
-		},
-		disarm: {
-			url: config.urls.disarm.url,
-			body: config.urls.disarm.body || ""
-		},
 		readCurrentState: {
 			url: config.urls.readCurrentState.url,
 			body: config.urls.readCurrentState.body || ""
@@ -141,7 +125,20 @@ function HttpSecuritySystemAccessory(log, config) {
 		readTargetState: {
 			url: config.urls.readTargetState.url,
 			body: config.urls.readTargetState.body || ""
+		},
+		setTargetStateOpen: {
+			url: config.urls.setTargetStateOpen.url,
+			body: config.urls.setTargetStateOpen.body || ""
+		},
+		setTargetStateClosed: {
+			url: config.urls.setTargetStateClosed.url,
+			body: config.urls.setTargetStateClosed.body || ""
 		}
+		//,
+		//readObstructionState: {
+		//	url: config.urls.readObstructionState.url,
+		//	body: config.urls.readObstructionState.body || ""
+		//}
 	};
 
 	self.httpMethod = config["http_method"] || "GET";
@@ -162,43 +159,36 @@ function HttpSecuritySystemAccessory(log, config) {
 /**
  * Initializer method, fired after the config has been applied
  */
-HttpSecuritySystemAccessory.prototype.init = function() {
+HttpGarageDoorAccessory.prototype.init = function() {
 	var self = this;
-
 	// set up polling if requested
 	if (self.polling) {
 		self.log("Starting polling with an interval of %s ms", self.pollInterval);
-
-		var emitterConfig = [
-			{
-				method: self.getCurrentState.bind(this),
-				property: 'current state',
-				characteristic: Characteristic.SecuritySystemCurrentState
-			},
-			{
-				method: self.getTargetState.bind(this),
-				logMessage: 'target state',
-				characteristic: Characteristic.SecuritySystemTargetState
-			}
-		];
-
-		emitterConfig.forEach(config => {
-			var emitter = pollingtoevent(function(done) {
-				config.method(function (err, result) {
-					done(err, result);
-				});
-			}, { longpolling: true, interval: self.pollInterval });
-
-			emitter.on("longpoll", function(state) {
-				self.log('Polling noticed %s change to %s, notifying devices', config.property, state);
-				self.securityService
-						.getCharacteristic(config.characteristic)
-						.setValue(state);
+		var emitter = pollingtoevent(function (done) {
+			self.getCurrentState(function (err, result) {
+				done(err, result);
 			});
+		}, { longpolling: true, interval: self.pollInterval });
 
-			emitter.on("err", function(err) {
-				self.log("Polling of %s failed, error was %s", property, err);
-			});
+		emitter.on("longpoll", function (state) {
+			self.log("Polling noticed status change to %s, notifying devices", state);
+			if (state == 2) {
+				self.garagedoorService
+				.getCharacteristic(Characteristic.TargetDoorState)
+				.setValue(0);
+				}
+			else if (state == 3) {
+				self.garagedoorService
+				.getCharacteristic(Characteristic.TargetDoorState)
+				.setValue(1);
+				}
+			self.garagedoorService
+				.getCharacteristic(Characteristic.CurrentDoorState)
+				.setValue(state);
+		});
+		
+		emitter.on("err", function(err) {
+			self.log("Polling failed, error was %s", err);
 		});
 	}
 };
@@ -210,7 +200,7 @@ HttpSecuritySystemAccessory.prototype.init = function() {
  * @param body The body of the request
  * @param callback Callback method to call with the result or error (error, response, body)
  */
-HttpSecuritySystemAccessory.prototype.httpRequest = function(url, body, callback) {
+HttpGarageDoorAccessory.prototype.httpRequest = function(url, body, callback) {
 	request({
 		url: url,
 		body: body,
@@ -234,33 +224,27 @@ HttpSecuritySystemAccessory.prototype.httpRequest = function(url, body, callback
  *
  * Only logs the message if the debug flag is on.
  */
-HttpSecuritySystemAccessory.prototype.debugLog = function () {
+HttpGarageDoorAccessory.prototype.debugLog = function () {
 	if (this.debug) {
 		this.log.apply(this, arguments);
 	}
 };
 
 /**
- * Sets the target state of the security device to a given state
+ * Sets the target state of the garagedoor device to a given state
  *
  * @param state The state to set
  * @param callback Callback to call with the result
  */
-HttpSecuritySystemAccessory.prototype.setTargetState = function(state, callback) {
-	this.log("Setting state to %s", state);
+HttpGarageDoorAccessory.prototype.setTargetState = function(state, callback) {
+	this.log("Setting target state to %s", state);
 	var cfg = null;
 	switch (state) {
-		case Characteristic.SecuritySystemTargetState.STAY_ARM:
-			cfg = this.urls.stay;
+		case Characteristic.TargetDoorState.OPEN:
+			cfg = this.urls.setTargetStateClosed;
 			break;
-		case Characteristic.SecuritySystemTargetState.AWAY_ARM :
-			cfg = this.urls.away;
-			break;
-		case Characteristic.SecuritySystemTargetState.NIGHT_ARM:
-			cfg = this.urls.night;
-			break;
-		case Characteristic.SecuritySystemTargetState.DISARM:
-			cfg = this.urls.disarm;
+		case Characteristic.TargetDoorState.CLOSED:
+			cfg = this.urls.setTargetStateOpen;
 			break;
 	}
 
@@ -287,7 +271,7 @@ HttpSecuritySystemAccessory.prototype.setTargetState = function(state, callback)
  * @param {string} string The string to apply the mappers to
  * @returns {string} The modified string after all mappers have been applied
  */
-HttpSecuritySystemAccessory.prototype.applyMappers = function(string) {
+HttpGarageDoorAccessory.prototype.applyMappers = function(string) {
 	var self = this;
 
 	if (self.mappers.length > 0) {
@@ -305,13 +289,13 @@ HttpSecuritySystemAccessory.prototype.applyMappers = function(string) {
 };
 
 /**
- * Gets the state of the security system from a given URL
+ * Gets the state of the garage door from a given URL
  *
  * @param {string} url The URL to poke for the result
  * @param {string} body The body of the request
  * @param {Function} callback The method to call with the results
  */
-HttpSecuritySystemAccessory.prototype.getState = function(url, body, callback) {
+HttpGarageDoorAccessory.prototype.getState = function(url, body, callback) {
 	if (!url) {
 		callback(null);
 	}
@@ -329,11 +313,11 @@ HttpSecuritySystemAccessory.prototype.getState = function(url, body, callback) {
 };
 
 /**
- * Gets the current state of the security system
+ * Gets the current state of the garage door
  *
  * @param {Function} callback The method to call with the results
  */
-HttpSecuritySystemAccessory.prototype.getCurrentState = function(callback) {
+HttpGarageDoorAccessory.prototype.getCurrentState = function(callback) {
 	var self = this;
 	self.debugLog("Getting current state");
 	this.getState(this.urls.readCurrentState.url, this.urls.readCurrentState.body, function(err, state) {
@@ -348,11 +332,11 @@ HttpSecuritySystemAccessory.prototype.getCurrentState = function(callback) {
 };
 
 /**
- * Gets the target state of the security system
+ * Gets the target state of the garage door
  *
  * @param {Function} callback The method to call with the results
  */
-HttpSecuritySystemAccessory.prototype.getTargetState =  function(callback) {
+HttpGarageDoorAccessory.prototype.getTargetState =  function(callback) {
 	var self = this;
 	self.debugLog("Getting target state");
 	this.getState(this.urls.readTargetState.url, this.urls.readTargetState.body, function(err, state) {
@@ -367,31 +351,31 @@ HttpSecuritySystemAccessory.prototype.getTargetState =  function(callback) {
 };
 
 /**
- * Identifies the security device (?)
+ * Identifies the garage door (?)
  *
  * @param {Function} callback The method to call with the results
  */
-HttpSecuritySystemAccessory.prototype.identify = function(callback) {
+HttpGarageDoorAccessory.prototype.identify = function(callback) {
 	this.log("Identify requested!");
 	callback();
 };
 
 /**
- * Returns the services offered by this security device
+ * Returns the services offered by this garage door
  *
  * @returns {Array} The services offered
  */
-HttpSecuritySystemAccessory.prototype.getServices =  function() {
-	this.securityService = new Service.SecuritySystem(this.name);
+HttpGarageDoorAccessory.prototype.getServices =  function() {
+	this.garagedoorService = new Service.GarageDoorOpener(this.name);
 
-	this.securityService
-		.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+	this.garagedoorService
+		.getCharacteristic(Characteristic.CurrentDoorState)
 		.on("get", this.getCurrentState.bind(this));
 
-	this.securityService
-		.getCharacteristic(Characteristic.SecuritySystemTargetState)
+	this.garagedoorService
+		.getCharacteristic(Characteristic.TargetDoorState)
 		.on("get", this.getTargetState.bind(this))
 		.on("set", this.setTargetState.bind(this));
 
-	return [ this.securityService ];
+	return [ this.garagedoorService ];
 };
